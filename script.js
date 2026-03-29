defa621aa34e222169c949ce0589a23d914bb143
Могильна Мілана <milanadem4uk535@gmail.com>
2026-03-29 19:18:17 +0300

    UpdateDesign2

--- script.js
+++ script.js
@@ -44,6 +44,21 @@ viewport.addEventListener('touchstart', onStart);
 window.addEventListener('touchmove', onMove);
 window.addEventListener('touchend', onEnd);
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
 document.querySelector('.contact-form').addEventListener('submit', async (e) => {
     e.preventDefault();
 
